992a7a6edde4ca821c8687ae5bcab64559e98f58
khj <kim37171@cuk.edu>
2021-12-24 21:05:11 +0900

    first commit

---
+++ src/test/java/com/khj/santaproject/SantaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khj.santaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
